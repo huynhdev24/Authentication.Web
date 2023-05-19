f980b047d992be2f8bb2bfbcbc2ef3157c07dcae
huynhdev24 <huynh.dev.24@gmail.com>
2023-05-19 13:59:26 +0700

    add Address model and implement AddressesController

---
+++ Authentication.Domain/Models/AddressModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Authentication.Domain.Models
+{
+    public class AddressModel
+    {
+        public string? Name { get; set; }
+        public string? HouseName { get; set; }
+        public string? City { get; set; }
+        public string? State { get; set; }
+        public int Pin { get; set; }
+    }
+}
---
+++ Authentication.Web/Controllers/AddressesController.cs
@@ -0,0 +1,26 @@
+﻿using Authentication.Domain.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Authentication.Web.Controllers
+{
+    [Authorize]
+    [Route("api/address")]
+    [ApiController]
+    public class AddressesController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<AddressModel> Get()
+        {
+            List<AddressModel> addresses = new()
+            {
+                new AddressModel { Name = "Nguyen Van Huynh", HouseName = "Wscenco", City = "HCM City", State = "HCM", Pin = 7100000 },
+                new AddressModel { Name = "Duong An", HouseName = "Gaming House", City = "HCM", State = "Kerala", Pin = 7100000 },
+                new AddressModel { Name = "Thanh An", HouseName = "Tiem Net", City = "Oachira", State = "HCM", Pin = 7100000 },
+            };
+            return addresses;
+        }
+    }
+}
