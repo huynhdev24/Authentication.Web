c522f943bf84e6f512098b44d9cb985a7ee3bbbe
huynhdev24 <huynh.dev.24@gmail.com>
2023-05-22 15:10:47 +0700

    implement ITaskRepository and ITodoRepository

---
+++ Authentication.Infrastructure/Repositories/ITaskRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Authentication.Domain.Entities;
+
+namespace Authentication.Infrastructure.Repositories
+{
+    public interface ITaskRepository: IGenericRepository<Domain.Entities.Task>
+    {
+    }
+}
